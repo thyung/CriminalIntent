803abe0f5698066f75fcbb3b5b53bca676a3a625
Yung Tin Ho <yungtinho@yahoo.com>
2017-01-16 01:24:42 +0800

    page184

--- app/src/main/java/com/example/thyung/criminalintent/Crime.java
+++ app/src/main/java/com/example/thyung/criminalintent/Crime.java
@@ -45,4 +45,9 @@ public class Crime {
     public void setSolved(boolean solved) {
         mSolved = solved;
     }
+
+    @Override
+    public String toString() {
+        return mTitle;
+    }
 }
--- app/src/main/java/com/example/thyung/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/thyung/criminalintent/CrimeListFragment.java
@@ -3,6 +3,10 @@ package com.example.thyung.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -11,6 +15,8 @@ import java.util.ArrayList;
  */
 
 public class CrimeListFragment extends ListFragment {
+    private static final String TAG = "CrimeListFragment";
+
     private ArrayList<Crime> mCrimes;
 
     @Override
@@ -18,5 +24,17 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crime_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter =
+                new ArrayAdapter<Crime>(getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        mCrimes);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = (Crime)getListAdapter().getItem(position);
+        Log.d(TAG, c.getTitle() + " was clicked");
     }
 }
