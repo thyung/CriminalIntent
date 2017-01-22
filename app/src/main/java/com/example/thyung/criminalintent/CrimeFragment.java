2e9ac3465e9115f3a26dd50a8bcbfff9ed0b9bfe
Yung Tin Ho <yungtinho@yahoo.com>
2017-01-22 15:24:18 +0800

    page216

--- app/src/main/java/com/example/thyung/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/thyung/criminalintent/CrimeFragment.java
@@ -3,6 +3,7 @@ package com.example.thyung.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -22,6 +23,7 @@ import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
     public static final String EXTRA_CRIME_ID = "com.example.thyung.criminalintent.crime_id";
+    private static final String DIALOG_DATE = "date";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -71,7 +73,14 @@ public class CrimeFragment extends Fragment {
         DateFormat df = new DateFormat();
         mDateButton.setText(df.format("EEEE, MMM dd, yyyy", mCrime.getDate()));
 
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(fm, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
---
+++ app/src/main/java/com/example/thyung/criminalintent/DatePickerFragment.java
@@ -0,0 +1,26 @@
+package com.example.thyung.criminalintent;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+
+/**
+ * Created by aeethyung on 1/22/2017.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setView(v)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
