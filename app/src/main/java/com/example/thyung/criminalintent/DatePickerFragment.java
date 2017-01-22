d8fe9a1950cfed7e73e09b5f62508208e0ba1712
Yung Tin Ho <yungtinho@yahoo.com>
2017-01-22 16:01:07 +0800

    page220

--- app/src/main/java/com/example/thyung/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/example/thyung/criminalintent/DatePickerFragment.java
@@ -6,17 +6,51 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 
 /**
  * Created by aeethyung on 1/22/2017.
  */
 
 public class DatePickerFragment extends DialogFragment {
+    public static final String EXTRA_DATE = "com.example.thyung.criminalintent.date";
+
+    private Date mDate;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_DATE, date);
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mDate = (Date)getArguments().getSerializable(EXTRA_DATE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
 
+        DatePicker datePicker = (DatePicker)v.findViewById(R.id.dialog_date_datePicker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mDate = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
+                // preserve selected valued on rotation
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+            }
+        });
         return new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.date_picker_title)
                 .setView(v)
